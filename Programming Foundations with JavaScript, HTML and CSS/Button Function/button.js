106f214add0ef5d6c3b3fb188e64a188d7b8af66
TheGuyNextToYou <subhamkhinchi018@gmail.com>
2020-07-13 00:27:58 +0530

    Add files via upload

---
+++ Programming Foundations with JavaScript, HTML and CSS/Button Function/button.js
@@ -0,0 +1,9 @@
+function applyoperation(){
+  var dd1 = document.getElementById("d1");
+  var dd2 = document.getElementById("d2");
+  
+  dd1.className = "applyond1";
+  dd2.className = "applyond2";
+  dd1.innerHTML = "Bonjour";
+  dd2.innerHTML = "bye bye";
+}
