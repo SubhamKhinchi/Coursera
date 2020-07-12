d97297c0ac1a655690ca736004935886b935ca5b
TheGuyNextToYou <subhamkhinchi018@gmail.com>
2020-07-13 01:28:02 +0530

    Add files via upload

---
+++ Programming Foundations with JavaScript, HTML and CSS/Button Function/Changing Background of Canvas with buttons/backgroundchange.js
@@ -0,0 +1,8 @@
+function dolime(){
+  var dd1 = document.getElementById("d1");
+  dd1.className = "changetoblue";
+}
+function doorange(){
+  var dd2 = document.getElementById("d2");
+  dd2.className = "changetoorange";
+}
