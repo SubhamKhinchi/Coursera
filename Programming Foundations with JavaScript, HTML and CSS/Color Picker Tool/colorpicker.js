f5d183630c506169a2078c71f8a954e6478c1256
TheGuyNextToYou <subhamkhinchi018@gmail.com>
2020-07-13 15:50:32 +0530

    Add files via upload

---
+++ Programming Foundations with JavaScript, HTML and CSS/Color Picker Tool/colorpicker.js
@@ -0,0 +1,10 @@
+function docolor(){
+  var dd1 = document.getElementById("d1");
+  var colorinput = document.getElementById("clr");
+  var color = colorinput.value;
+  dd1.style.backgroundColor = color;
+}
+function dolime(){
+  var dd1 = document.getElementById("d1");
+  dd1.style.backgroundColor = "Blue";
+}
